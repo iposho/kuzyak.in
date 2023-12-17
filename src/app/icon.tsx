30165e1824aeea666a5c1787d94333d640247b72
Pavel Kuzyakin <p.kuzyakin@eapteka.ru>
2023-12-17 19:10:08 +0400

    Icon and sitemap

---
+++ src/app/icon.tsx
@@ -0,0 +1,50 @@
+import { ImageResponse } from 'next/og';
+
+export const runtime = 'edge';
+
+export const size = {
+  width: 32,
+  height: 32,
+};
+export const contentType = 'image/png';
+
+export default async function Icon() {
+  const image = await fetch(new URL('/public/og.png', import.meta.url)).then(
+    (res) => res.arrayBuffer(),
+  );
+
+  const dataUrl = `data:${contentType};base64,${Buffer.from(image).toString('base64')}`;
+
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <div
+        style={{
+          fontSize: 24,
+          background: 'transparent',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          color: 'white',
+        }}
+      >
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          alt=""
+          src={dataUrl}
+          width={32}
+          height={32}
+          style={{
+            backgroundColor: 'transparent',
+            boxShadow: '8px 8px 45px -15px #000',
+          }}
+        />
+      </div>
+    ),
+    {
+      ...size,
+    },
+  );
+}
