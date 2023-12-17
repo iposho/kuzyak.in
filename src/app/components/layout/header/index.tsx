bd1a431c1470d392083053070147aa583b9be440
Pavel Kuzyakin <p.kuzyakin@eapteka.ru>
2023-12-17 13:39:13 +0400

    New Christmas hat

--- src/app/components/layout/header/index.tsx
+++ src/app/components/layout/header/index.tsx
@@ -5,14 +5,28 @@ import css from './header.module.scss';
 export default function Header() {
   return (
     <header className={css.header}>
-      <Image
-        src="/me.webp"
-        alt="Павел Кузякин"
-        width={150}
-        height={150}
-        className={css.logo}
-        priority
-      />
+      <div className={css.image}>
+        <Image
+          src="/me.webp"
+          alt="Павел Кузякин"
+          width={150}
+          height={150}
+          className={css.logo}
+          priority
+        />
+        {
+          (new Date() <= new Date('2024-01-07'))
+          && (
+            <Image
+              className={css.hat}
+              src="/hat.webp"
+              width={120}
+              height={120}
+              alt=""
+            />
+          )
+        }
+      </div>
       <h1 className={css.title}>Павел Кузякин</h1>
     </header>
   );
