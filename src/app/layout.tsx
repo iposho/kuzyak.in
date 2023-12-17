5ee62d9e1c078b13b4ef871ab851b1ddc275ae18
Pavel Kuzyakin <p.kuzyakin@eapteka.ru>
2023-12-18 01:27:19 +0400

    Themes: first draft

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,12 @@
 import type { Metadata } from 'next';
 import { ReactNode } from 'react';
 import { Noto_Sans } from 'next/font/google';
-
 import {
   METADATA_BASE, METADATA_DESCRIPTION, METADATA_SITE_NAME, METADATA_TITLE,
 } from '@/app/constants/metadata';
+
+import ThemeProvider from './theme-provider';
+import ThemeSwitcher from './components/ui/themeSwitcher';
 
 import './styles/globals.scss';
 
@@ -29,9 +31,12 @@ export default function RootLayout({
   children: ReactNode
 }) {
   return (
-    <html lang="ru">
+    <html lang="ru" suppressHydrationWarning>
       <body className={notoSans.className}>
-        {children}
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <ThemeSwitcher />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
