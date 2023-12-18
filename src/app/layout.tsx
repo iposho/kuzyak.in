74c80d40061eb094ffa55520c201022772297cbc
Pavel Kuzyakin <p.kuzyakin@eapteka.ru>
2023-12-18 22:27:56 +0400

    Yandex metrika analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import {
   METADATA_BASE, METADATA_DESCRIPTION, METADATA_SITE_NAME, METADATA_TITLE,
 } from '@/app/constants/metadata';
 
+import Script from 'next/script';
+
 import ThemeProvider from './theme-provider';
 import ThemeSwitcher from './components/ui/themeSwitcher';
 
@@ -30,15 +32,37 @@ export default function RootLayout({
 }: {
   children: ReactNode
 }) {
+  const isProduction = process.env.NODE_ENV === 'production';
+
   return (
     <html
       lang="ru"
       suppressHydrationWarning
     >
       <body className={notoSans.className}>
+        {
+          isProduction
+          && (
+            <Script id="metrika-counter" strategy="afterInteractive">
+              {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(${process.env.YANDEX_METRIKA_ID}, "init", {
+              defer: true,
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true,
+              webvisor:true
+            });
+            console.log('YMetrika init');`}
+            </Script>
+          )
+        }
         <ThemeProvider
           attribute="data-theme"
-          defaultTheme="system"
           enableSystem
         >
           <ThemeSwitcher />
