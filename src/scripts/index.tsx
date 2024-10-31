db54015ef9ec236f828b44cac442f2e3839b4d1c
Pavel Kuzyakin <iposho@gmail.com>
2024-11-01 02:40:06 +0400

    New light version

--- src/scripts/index.tsx
+++ src/scripts/index.tsx
@@ -1,11 +1,11 @@
 import { ReactElement } from 'react';
 
-import { YandexMetrika } from './YandexMetrika';
+import { Metrika } from './YandexMetrika';
 
 function Scripts():ReactElement {
   return (
     <>
-      <YandexMetrika />
+      <Metrika />
     </>
   );
 }
