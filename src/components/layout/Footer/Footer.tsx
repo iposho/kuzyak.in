33ffacac1b0b567d39dbfc92a429c91fb48e3828
Pavel Kuzyakin <40692570+iposho@users.noreply.github.com>
2024-11-18 00:02:46 +0400

    fix (NO TASK) New metadata

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import { ReactElement } from 'react';
+import { usePathname } from 'next/navigation';
+import Image from 'next/image';
 
 import createDateDiapason from '@/helpers/dates';
 
@@ -12,22 +14,40 @@ import {
 
 import css from './Footer.module.scss';
 
-export const Footer = (): ReactElement => (
-  <footer className={css.footer}>
-    <div>
-      <p className={css.copyright}>
-        {`© ${createDateDiapason(COPYRIGHT_START_YEAR, '...')}`}
-      </p>
-      <p className={css.version}>
-        <a
-          href={REPOSITORY_URL}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          v.
-          {VERSION}
-        </a>
-      </p>
-    </div>
-  </footer>
-);
+export const Footer = (): ReactElement => {
+  const pathname = usePathname();
+  const isHomePage = pathname === '/';
+
+  return (
+    <>
+      <footer className={css.footer}>
+        <div>
+          <p className={css.copyright}>
+            {`© ${createDateDiapason(COPYRIGHT_START_YEAR, '...')}`}
+          </p>
+          <p className={css.version}>
+            <a
+              href={REPOSITORY_URL}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              v.
+              {VERSION}
+            </a>
+          </p>
+        </div>
+      </footer>
+      {
+        isHomePage && (
+          <Image
+            className={css.face}
+            src="/images/face-nobg.webp"
+            width={200}
+            height={200}
+            alt=""
+          />
+        )
+      }
+    </>
+  );
+};
