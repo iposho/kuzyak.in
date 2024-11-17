03682d2d602cc57b2e08b2c81a876d5ae5535385
Pavel Kuzyakin <40692570+iposho@users.noreply.github.com>
2024-11-17 22:53:45 +0400

    fix (NO TASK) New metadata

--- src/constants/base.ts
+++ src/constants/base.ts
@@ -5,12 +5,10 @@ import packageJson from '../../package.json';
 // Metadata
 export const METADATA_BASE = new URL('https://kuzyak.in');
 export const METADATA_TITLE = 'Павел Кузякин';
-export const METADATA_ROLE = 'Фронтенд-разработчик';
 export const METADATA_DESCRIPTION = 'Я фронтенд-разработчик. '
   + 'Люблю создавать сложные веб-сайты и приложения; нанимать, обучать и развивать людей.';
 export const METADATA_KEYWORDS = SKILLS_STRING;
 
-export const OPEN_TO_WORK = false;
 export const VERSION = packageJson?.version || '0.0.0';
 export const REPOSITORY_URL = packageJson?.repository?.url || 'https://github.com/iposho/kuzyak.in';
 
