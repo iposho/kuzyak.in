45175b29739aea7b1ab120874da0d8684f267813
Pavel Kuzyakin <40692570+iposho@users.noreply.github.com>
2025-02-13 20:30:20 +0400

    feat(footer): add branded social media icons with hover effects

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,8 +1,10 @@
 'use client';
 
 import { ReactElement } from 'react';
+import Link from 'next/link';
 
 import createDateDiapason from '@/helpers/dates';
+import { SOCIAL_LINKS } from '@/constants/social';
 
 import {
   COPYRIGHT_START_YEAR,
@@ -15,6 +17,26 @@ import css from './Footer.module.scss';
 export const Footer = (): ReactElement => (
   <footer className={css.footer}>
     <div>
+      <ul className={css.socialLinks}>
+        {SOCIAL_LINKS.map(({
+          id,
+          link,
+          label,
+          icon: Icon,
+        }) => (
+          <li key={id}>
+            <Link
+              href={link}
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label={label}
+              className={css.socialLink}
+            >
+              <Icon size={36} />
+            </Link>
+          </li>
+        ))}
+      </ul>
       <p className={css.copyright}>
         {`© ${createDateDiapason(COPYRIGHT_START_YEAR, '...')}`}
       </p>
--- src/constants/social.ts
+++ src/constants/social.ts
@@ -1,3 +1,11 @@
+import {
+  FaGithub,
+  FaLinkedin,
+  FaFacebook,
+  FaTwitter,
+  FaTelegram,
+} from 'react-icons/fa';
+
 export enum SocialPlatform {
   Facebook = 'Facebook',
   Linkedin = 'Linkedin',
@@ -10,6 +18,7 @@ export interface ISocialLink {
   id: number;
   link: string;
   label: SocialPlatform;
+  icon: typeof FaGithub;
 }
 
 export const SOCIAL_LINKS: ISocialLink[] = [
@@ -17,25 +26,30 @@ export const SOCIAL_LINKS: ISocialLink[] = [
     id: 1,
     link: 'https://facebook.com/pashakuzyakin',
     label: SocialPlatform.Facebook,
+    icon: FaFacebook,
   },
   {
     id: 2,
     link: 'https://www.linkedin.com/in/pavelkuzyakin/',
     label: SocialPlatform.Linkedin,
+    icon: FaLinkedin,
   },
   {
     id: 3,
     link: 'https://twitter.com/pavelkuzyakin',
     label: SocialPlatform.Twitter,
+    icon: FaTwitter,
   },
   {
     id: 4,
     link: 'https://github.com/iposho',
     label: SocialPlatform.Github,
+    icon: FaGithub,
   },
   {
     id: 5,
     link: 'https://telegram.me/mrPosho',
     label: SocialPlatform.Telegram,
+    icon: FaTelegram,
   },
 ];
