4c8e00b94232352831df46c1223840fe6a025bbf
Pavel Kuzyakin <40692570+iposho@users.noreply.github.com>
2025-04-18 06:57:28 +0400

    feat: добавлена секция с информацией о местоположении

--- src/helpers/generateRandomColor.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Generates a random color in hexadecimal format.
- *
- * @returns {string} The random color in hexadecimal format (e.g. "#a0522d").
- */
-
-export default function generateRandomColor(): string {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
