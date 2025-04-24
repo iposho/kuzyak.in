7d5db2dd028e54e06460b2807a189c46be768014
Pavel Kuzyakin <40692570+iposho@users.noreply.github.com>
2025-04-25 03:22:52 +0400

    Add console log for app version in Footer component
    Introduced a `useEffect` hook to log the app version to the console upon rendering the Footer. This change helps in tracking the deployed version during development or debugging. No changes were made to the visual output of the component.
    
    Signed-off-by: Pavel Kuzyakin <40692570+iposho@users.noreply.github.com>

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,48 +1,59 @@
 'use client';
 
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 
 import Image from 'next/image';
 
 import {
   LAST_UPDATE_DATE,
+  VERSION,
 } from '@/constants/base';
 
 import { formatDate } from '@/utils/formatDate';
 
 import css from './Footer.module.scss';
 
-export const Footer: FC = () => (
-  <footer className={css.footer}>
-    <div className={css.column}>
-      <p className={css.copyright}>
-        {formatDate(new Date(LAST_UPDATE_DATE))}
-      </p>
+export const Footer: FC = () => {
+  useEffect(() => {
+    // eslint-disable-next-line no-console
+    console.log(
+      `%c🚀 Версия ${VERSION}`,
+      'color: #4CAF50; font-weight: bold;',
+    );
+  }, []);
 
-      {/* <p className={css.version}>
+  return (
+    <footer className={css.footer}>
+      <div className={css.column}>
+        <p className={css.copyright}>
+          {formatDate(new Date(LAST_UPDATE_DATE))}
+        </p>
+
+        {/* <p className={css.version}>
+          <a
+            href={REPOSITORY_URL}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            v.
+            {VERSION}
+          </a>
+        </p> */}
+
         <a
-          href={REPOSITORY_URL}
+          href="https://web.archive.org/web/20010406054522/http://mir.glasnet.ru/~awicon/"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noreferrer"
         >
-          v.
-          {VERSION}
+          <Image
+            src="/images/awicons/temafree.webp"
+            alt="Tema Lebedev Free Website"
+            className={css.banner}
+            width={100}
+            height={12}
+          />
         </a>
-      </p> */}
-
-      <a
-        href="https://web.archive.org/web/20010406054522/http://mir.glasnet.ru/~awicon/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <Image
-          src="/images/awicons/temafree.webp"
-          alt="Tema Lebedev Free Website"
-          className={css.banner}
-          width={100}
-          height={12}
-        />
-      </a>
-    </div>
-  </footer>
-);
+      </div>
+    </footer>
+  );
+};
